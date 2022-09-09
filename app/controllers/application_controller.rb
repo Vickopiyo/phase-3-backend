a599fc75544285e3ef0c428577290e798a5b6e38
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-09 11:02:54 +0300

    SERVER

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,33 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/foods" do
+    products = Food.all
+    products.to_json(include: :reviews)
+  end
+  get "/customers" do
+    customers = Customer.all
+    customers.to_json
+  end
+  get "/reviews" do
+    reviews= Review.all
+    reviews.to_json
+  end
+  post "/reviews" do
+    review = Review.create(comment: params[:comment], foodtype: params[:foodtype])
+    review.to_json
+  end
+  
+  patch "/reviews/:id" do
+    review= Review.find(params[:id])
+    review.update(comment: params[:comment])
+    review.to_json
+  end
+  
+  delete "/foods/:id" do
+    food = Food.find(params[:id])
+    food.destroy
+    food.to_json
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding data..."
 
-# Seed your database here
+Food.create(foodtype:"Pilau", price:400, imageurl: "https://images.unsplash.com/photo-1593422088556-e2e3f35d94a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80ttps://unsplash.com/photos/NtZhLVG9oL4")
+Food.create(foodtype:"Nyama Fry", price: 700, imageurl: "https://images.unsplash.com/photo-1604908177453-7462950a6a3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=757&q=80")
+Food.create(foodtype:"Pweza", price: 550, imageurl: "https://images.unsplash.com/photo-1606850780554-b55ea4dd0b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")
+Food.create(foodtype:"Mkwaju", price: 350, imageurl: "https://images.unsplash.com/photo-1622597467821-df79dcb4f94d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80")
+
+Customer.create(name: "Kevin")
+Customer.create(name: "Dennis")
+Customer.create(name: "Daddy")
+Customer.create(name: "Brian")
+Customer.create(name: "Simon")
+Customer.create(name: "Kamau")
+Review.create(comment: "awesome meal",customer_id: 1, food_id: 2)
+Review.create(comment: "superb meal",customer_id: 1, food_id: 1)
+
+Review.create(comment: "great meal",customer_id: 2, food_id: 1)
+Review.create(comment: "nice meal",customer_id: 1, food_id: 3)
+Review.create(comment: "orgasmic meal",customer_id: 4, food_id: 3)
+Review.create(comment: "great meal",customer_id: 3, food_id: 1)
+Review.create(comment: "wow! meal",customer_id: 5, food_id: 1)
+Review.create(comment: "great meal",customer_id: 5, food_id: 2)
+Review.create(comment: "loving this meal!",customer_id: 2, food_id: 2)
+Review.create(comment: "Heavenly meal",customer_id: 2, food_id: 3)
+
+
 
 puts "✅ Done seeding!"
